0979ad94b71649f01e0754d51a8da5961b8516c5
wexoffe <142429344+wexoffe@users.noreply.github.com>
2024-07-18 21:06:33 +0300

    task1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -4,16 +4,45 @@ package com.walking.intensive.chapter1.task1;
  * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
  */
 public class Task1 {
+
+    static int[] exceptions = {11, 12, 13, 14, 111, 112, 113, 114};
+
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
         int age = 0;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
+        String ageString = "Вам ";
 
-        return null; // Заглушка. При реализации - удалить
+        if(isException(age) || endsWith(age, new int[] {0, 5, 6, 7, 8, 9})) {
+            ageString += age + " лет";
+        } else if(endsWith(age, new int[] {2, 3, 4})) {
+            ageString += age + " года";
+        } else {
+            ageString += age + " год";
+        }
+
+        return ageString;
+    }
+
+    static boolean isException(int age) {
+        for(int i = 0; i < exceptions.length; i++) {
+            if(exceptions[i] == age) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean endsWith(int age, int[] lastChar) {
+        for(int i = 0; i < lastChar.length; i++) {
+            if(age%10 == lastChar[i]) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
